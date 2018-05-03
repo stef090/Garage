342a298744e97b13ee31dc3599e21c0a8963548b
Stjepan Lovasić <stjepan.lovasic@intis.hr>
2018-05-03 13:16:27 +0200

    Test passes for smaller numbers, need to fix for larger ranges

--- JavaScript/leastCommonMultiple.js
+++ JavaScript/leastCommonMultiple.js
@@ -1,44 +1,62 @@
 function smallestCommons(arr) {
-  divisors = {};
-  range = generateRange(arr);
+  var divisors = {};
+  var repetitions = {};
+  var range = generateRange(arr);
   range.forEach(function(element)
-  {
-    division = []
-    if(element==1)
     {
-      division.push(element);
-      divisors[element] = division;
-    }
-    else
-    {
-      // Start with 2 because it is the smallest prime number which can divide another number;
-      divisor=2;
-      original_value = element;
-      while (element>1)
+      var division = []
+      if(element==1)
       {
-        if (element%divisor==0)
-        {
-            division.push(divisor);
-            element /= divisor;
-        }
-        else
-        {
-          divisor++;
-        }
+        division.push(element);
+        divisors[element] = division;
       }
-      divisors[original_value] = division;
+      else
+      {
+        // Start with 2 because it is the smallest prime number which can divide another number;
+        // Take the original value for writing to divisor dictionary
+        // Count repetitions of each divisor and record to repetitions by divisor.
+        // If there is a higher repetition then overwrite the current number.
+        var divisor=2;
+        var original_value = element;
+        var repetition = 1;
+        while (element>1)
+        {
+          if (element%divisor==0)
+          {
+              division.push(divisor);
+              element /= divisor;
+              if (repetitions[divisor] && repetition > repetitions[divisor])
+                {
+                  repetitions[divisor] = repetition;
+                }
+              else if (!repetitions[divisor])
+              {
+                repetitions[divisor] = repetition;
+              }
+              repetition++;
+          }
+          else
+          {
+            divisor++;
+            repetition = 1;
+          }
+        }
+        divisors[original_value] = division;
+      }
     }
-  }
-)
-  console.log(divisors);
-  return arr;
+  );
+  factors = Object.keys(repetitions).map(function(item){
+    return parseInt(item, 10);
+  });
+  factor_repetitions = Object.values(repetitions);
+  return multiplyFactors(factors, factor_repetitions);
 }
 
 function generateRange(arr)
 {
-  range=[]
-  smaller=arr[0];
-  greater=arr[1];
+  var range=[];
+  var smaller=arr[0];
+  var greater=arr[1];
   if (arr[1]<arr[0])
   {
     greater=arr[0];
@@ -51,5 +69,16 @@ function generateRange(arr)
   return range;
 }
 
+function multiplyFactors(factors, repetitions)
+{
+  var result = 1;
+  for(var i=0;i<factors.length;i++)
+  {
+    result*=factors[i]*repetitions[i];
+  }
+  console.log(result);
+  return result;
+}
 
-smallestCommons([1,5]);
+
+smallestCommons([1,13]);
